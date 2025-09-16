adcaeecedfa34f43462baa5d3e716078ac035395
CauanSPrado <cauandoprado18@gmail.com>
2025-09-15 21:59:07 -0300

    Delete CategoryRepository.java

--- CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todolist.repository;
-
-import com.example.todolist.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
