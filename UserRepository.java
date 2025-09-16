8028e484a6d6491b3b935c3742de1a78f06b86df
CauanSPrado <cauandoprado18@gmail.com>
2025-09-15 21:59:38 -0300

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todolist.repository;
-
-import com.example.todolist.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
